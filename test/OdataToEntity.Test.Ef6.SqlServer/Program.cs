be5582acd9c49cb51dbc0d0369b85454d737d7a3
voronov-maxim <vornov1972@yandex.ru>
2017-03-06 16:20:47 +0300

    add ef6 test project

---
+++ test/OdataToEntity.Test.Ef6.SqlServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdataToEntity.Test.Ef6.SqlServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
